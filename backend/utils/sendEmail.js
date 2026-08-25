cebdaaf59a64a3f722d274759e83cd495f5b5b20
SreejaKodaparthi <sreejakodaparthi>
2026-08-25 07:24:25 +0530

    updated sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,25 +1,43 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: Number(process.env.EMAIL_PORT),
+//     secure: process.env.EMAIL_PORT === "465",
+
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: `"Employee Skill Discovery Platform" <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     html: options.html,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
+
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: process.env.EMAIL_PORT === "465",
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
-    from: `"Employee Skill Discovery Platform" <${process.env.EMAIL_USER}>`,
+  const { error } = await resend.emails.send({
+    from: "Employee Skill Discovery Platform <onboarding@resend.dev>",
     to: options.email,
     subject: options.subject,
     html: options.html,
-  };
+  });
 
-  await transporter.sendMail(mailOptions);
+  if (error) {
+    throw new Error(error.message || "Failed to send email");
+  }
 };
 
 module.exports = sendEmail;
